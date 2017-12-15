4213e6bc88903f2168054a9d27efa17e23d90678
Cynthia Destiani <cynthia.destiani@gmail.com>
2017-12-15 14:06:53 +0700

    Model Search pergikuliner

---
+++ backend/openrice.py
@@ -0,0 +1,138 @@
+import json
+import re
+import requests
+from bs4 import BeautifulSoup
+restaurant = raw_input("Restaurants you want to find? ")
+
+
+def search_restaurant(query):
+    # For accessing openrice search
+    url = "id.openrice.com/en/jakarta/restaurants?what="+restaurant
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+    r = requests.get("http://" +url, headers=headers)
+    search = r.text
+
+    soup = BeautifulSoup(search, 'html.parser')
+    url = "id.openrice.com"
+
+    #Getting number of pages
+    page = int(soup.find('div', class_="js-dishFilter").find('li').get('data-count'))
+    page = (page / 15) + 1
+
+    #Getting all result to map
+    search_result = {}
+    count = 1
+    while count <= page:
+        url = "id.openrice.com/en/jakarta/restaurants?what="+restaurant+"&page="+str(count)
+        r = requests.get("http://" +url, headers=headers)
+        search = r.text
+        soup = BeautifulSoup(search, 'html.parser')
+        for a in soup.find_all('li', class_='pois-restaurant-list-cell'):
+            details = {}
+            name = a.find('h2', class_='title-name').get_text().strip()
+            if not name:
+                name = ""
+            alamat = a.find('div', class_='address')
+            if not alamat:
+                alamat = ""
+            else:
+                details['alamat'] = alamat.find('span').get_text().strip()
+                alamat = alamat.find('span').get_text().split(',')[0].strip()
+                alamat = re.sub('Mall | Mall|mall | mall', "", alamat)
+            review = a.find('div', class_='counters-container')
+            if not review:
+                review = 0
+            else:
+                review = int(re.findall(re.compile(r'[0-9]+'), review.find('span').get_text())[0])
+            details['review'] = review
+            good = a.find('div', class_='smile-face')
+            rating = 0
+            if good:
+                good = int(good.find('span').get_text())
+                bad = int(a.find('div', class_='sad-face').find('span').get_text())
+                ok = review - good + bad
+                rating = round(((7.5 * good) + (5 * ok) + (2.5 * bad))/review, 1)
+            details['rating'] = rating
+            search_result[name + " - " + alamat] = details
+
+        count = count + 1
+    return search_result
+
+def see_details(name):
+    # For accessing openrice detail from search hit
+    name = name.split('-')
+    url = "id.openrice.com/en/jakarta/restaurants?what="+name[0].strip()+"&where="+name[1].strip()
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+    r = requests.get("http://" +url, headers=headers)
+    search = r.text
+    soup = BeautifulSoup(search, 'html.parser')
+    url = "http://id.openrice.com"
+    data = soup.find('li', class_='pois-restaurant-list-cell')
+    if not data:
+        return "N/A"
+    url += data.find('a').get('href')
+    soup = BeautifulSoup (r.text, 'html.parser')
+    #Taking static data from the first result
+    url += soup.find('h2', class_='title-name').find('a').get('href')
+    r = requests.get("http://"+url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    #Taking name data
+    data = json.loads(soup.find('script', type='application/ld+json').text)
+    name = data['name'] #Save name
+    print name
+
+    cost = data['priceRange']   #Save average cost
+    pattern = re.compile(r'([0-9]+).([0-9]+)')
+    avg_cost = 0
+    for (ribuan, ratusan) in re.findall(pattern, cost):
+        avg_cost += int(ribuan+""+ratusan)
+
+    avg_cost /= 2
+    print avg_cost
+
+    jenis = soup.find('meta', property='og:description').get('content') #Save restaurant type
+    jenis_resto = []
+    pattern = re.compile(r'[a-zA-Z]+')
+    for x in re.findall(pattern, jenis):
+        jenis_resto.append(x)
+
+    def filterReviews(element):
+      return element != "reviews"
+
+    jenis_resto = filter(filterReviews, jenis_resto)
+    print jenis_resto
+
+    sign_dish = []
+    dish = soup.find('section', class_='signature-dishes-section')
+    if dish:
+        dish = dish.find('div', class_='slash-tags')
+        for sign_dishes in dish:
+            sign_dish.append(sign_dishes.string.strip())
+
+    sign_dish = filter(None, sign_dish)
+    print sign_dish
+
+    script = soup.find('script', type='application/ld+json')
+    if script:
+        data = json.loads(script.text)
+
+        #Getting address data
+        data_add = data['address']
+        address = data_add['streetAddress']+" "+data_add['addressLocality']+" "+data_add['addressRegion']+" "+data_add['postalCode']
+        print address.encode("utf-8")
+
+        #Getting telephone
+        telephone = data['telephone']
+        print telephone.encode("utf-8")
+
+    facilities = []
+    for avail_facil in soup.find_all('div', class_='condition-item'):
+        if avail_facil.find('span', class_='or-sprite-inline-block d_sr2_lhs_tick_desktop'):
+            facilities.append(avail_facil.find('span', class_='condition-name').string)
+    print facilities
+
+    rating = soup.find('div', class_='header-score-details-left-score', itemprop='ratingValue')
+    print rating
+    
+print search_restaurant(restaurant)
---
+++ backend/scrapingpergikuliner.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import sys
+import string
+import re
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+r_input = raw_input("masukkan nama restoran: ")
+#r_input = "holycow steak"
+if len(r_input.split()) > 1:
+	r_name = string.replace(r_input, ' ', '+')
+else:
+	r_name = r_input
+
+def search_restaurant(query):
+	tmpurl = ("https://pergikuliner.com/restaurants?utf8=✓&search_place=&default_search=&search_name_cuisine={}".format(query))
+	headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+	tmppage = requests.get(tmpurl, headers=headers)
+	tmpcontent = tmppage.text
+	tmpsoup = BeautifulSoup(tmpcontent, 'html.parser')
+	pages = tmpsoup.find('h2', id='top-total-search-view').get_text().strip()
+	pattern = re.compile(r'[0-9]+')
+	pages = int(re.findall(pattern, pages)[1]) / 12
+
+	search_result = {}
+	count = 1
+	while count <= pages:
+		tmpcontent = requests.get("https://pergikuliner.com/restaurants?default_search=jakarta&page="+ str(count) +"&search_name_cuisine="+ query +"&search_place=", 
+			headers=headers).text
+		soup = BeautifulSoup(tmpcontent, 'html.parser')
+		for data, rat in zip(soup.find_all('div', class_="item-info"), soup.find_all('div', class_='item-rating-result')):
+			details = {}
+			name = data.find('h3', class_="item-name").get_text().strip()
+			alamat = data.find('p', class_="clearfix").find_all('span', class_='truncate')
+			alamat = ", ".join([Alamat.get_text().encode("utf-8").strip() for Alamat in data.find('p', class_="clearfix").find_all('span', class_='truncate')])
+			rating = float(rat.get_text().strip().split('/')[0])
+			details['alamat'] = alamat
+			details['rating'] = rating
+			search_result[name + " - " + re.sub('Mall | Mall|mall | mall', "", alamat.split(",")[0])] = details
+		count += 1
+	return search_result
+
+
+def see_details(name):	
+	#print "inputnya sekarang jadi " + r_name
+	#url = "https://pergikuliner.com/restaurants?utf8=✓&search_place=&default_search=Jakarta&search_name_cuisine=cafe"
+	tmpurl = ("https://pergikuliner.com/restaurants?utf8=✓&search_place=&default_search=&search_name_cuisine={}".format(r_name))
+	headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+	tmppage = requests.get(tmpurl, headers=headers)
+	tmpcontent = tmppage.text
+	tmpsoup = BeautifulSoup(tmpcontent, 'html.parser')
+	links = tmpsoup.find_all('a', href=True)
+
+	url = "https://pergikuliner.com" + links[33]['href']
+	page = requests.get(url, headers=headers)
+	content = page.text
+	soup = BeautifulSoup(content, 'html.parser')
+
+
+
+	#print(soup.prettify())
+	place = soup.find_all(itemprop="name")
+	province = place[1].string
+	city = place[2].string
+	street = place[3].string
+	address = soup.find("article").find("p").find("span").get_text().strip()
+	ratingOverall = soup.find(itemprop = "ratingValue").string #rating
+	cuisine = soup.find(itemprop = "servesCuisine").string #jenis restoran
+	telephone = soup.find(itemprop = "telephone").find("a").string #no telfon
+	pricerange = soup.find(itemprop = "priceRange").string.strip() #range harga
+	openhours = soup.find(itemprop = "openingHours").string #waktu buka
+	payment = soup.find(itemprop = "paymentAccepted") #jenis pembayaran
+	hasfacilities = soup.find_all(class_ = "checked") #list fasilitas
+	notfacilities = soup.find_all(class_ = "unchecked") #list bukan fasilitas
+	recommenu = soup.find(class_ = "tbody").find_all("a") #recommended menu, tp yg keluar masih top reviewer aja
+
+	print "alamat " + address;
+	print province + " " + city + " " + street;
+	print "rating " + ratingOverall;
+	print "cuisine " + str(cuisine);
+	print "no telfon " + telephone;
+	print "range harga " + pricerange;
+	print "jam buka " + openhours;
+	print "pembayaran " + payment['content'];
+
+	print "punya fasilitas:";
+	for f in hasfacilities:
+	   print f.string;
+	print "gapunya fasilitas:";
+	for nf in notfacilities:
+	   print nf.string;
+	#for m in recommenu:
+	#   print m.string;
+
+	# related restaurant by location
+	baseurlloc = "https://pergikuliner.com/restaurants?utf8=✓&search_place="+ street + "&default_search=&search_name_cuisine="
+	pageloc = requests.get(baseurlloc, headers=headers)
+	contentloc = pageloc.text
+	souploc = BeautifulSoup(contentloc, 'html.parser')
+
+	restolists = souploc.find_all(class_="item-info")
+
+	print "list of restaurants related to " + r_input + " based on location:";
+	for rl in restolists:
+		print rl.find("a").string + " " + rl.find_all("div")[1].string.strip();
+
+
+	# related restaurant by food
+	baseurlfood = "https://pergikuliner.com/restaurants?utf8=✓&search_place=&default_search=&search_name_cuisine=" + cuisine
+	pagefood = requests.get(baseurlfood, headers=headers)
+	contentfood = pagefood.text
+	soupfood = BeautifulSoup(contentfood, 'html.parser')
+
+	restolists2 = soupfood.find_all(class_="item-info")
+	print "\n";
+	print "list of restaurants related to " + r_input + " based on cuisine:";
+	for rl2 in restolists2:
+		print rl2.find("a").string + " " + rl2.find_all("div")[1].string.strip();
+print search_restaurant(r_name)
