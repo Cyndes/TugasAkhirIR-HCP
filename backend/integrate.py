681bfff31466bc3acb76e6decd5ce07e1a1011af
Tengku Huday Ilahi <tengkuhuday@gmail.com>
2017-12-15 15:19:35 +0700

    adjsut name

--- backend/integrate.py
+++ backend/integrate.py
@@ -1,99 +1,20 @@
+from scrappingzomato import *
+from scrappingopenrice import *
+from scrappingfoody import *
+from scrappingpergikuliner import *
+
 def integrate(query):
 	# panggil search zomato
-	#result_zomato = search_zomato(query)
+	#result_zomato = scrappingzomato.seach_restaurant(query)
 	# panggil search openrice
-	#result_openrice = search_openrice(query)
+	result_openrice = scrappingopenrice.seach_restaurant(query)
 	# panggil search foody
-	#result_foody = search_foody(query)
+	result_foody = scrappingfoody.seach_restaurant(query)
 	# panggil search pergikuliner
-	#result_pergikuliner = seach_pergi_kuliner(query)
+	result_pergikuliner = scrappingpergikuliner.seach_restaurant(query)
 
-	lovely_dummy_zomato = {'KFC - Pondok Indah': 
-	   {'rating': 3.5, 
-	    'review': 12, 
-	    'address': "Pondok Indah Mall 2, Lt. 3, No. 14"
-	   },
-	 'McDonald - Jl. Margonda No. 23': 
-	   {'rating': 4.6, 
-	    'review': 24, 
-	    'address': "Jl. Margonda No. 23, Beji, Depok"
-	   },
-	 'Pizza Hut - Jl. Margonda No. 7': 
-	   {
-	    'rating': 3.2, 
-	    'review': 15, 
-	    'address': "Jl. Margonda No.7, Depok"
-	   }
-	}
-
-	lovely_dummy_openrice = {'KFC - Pondok Indah': 
-	   {'rating': 7.6, 
-	    'review': 7, 
-	    'address': "Pondok Indah Mall 2, Lt. 3, No. 14"
-	   },
-	 'McDonald - Jl. Margonda No. 23': 
-	   {'rating': 7.6, 
-	    'review': 8, 
-	    'address': "Jl. Margonda No. 23, Beji, Depok"
-	   },
-	 'Pizza Hut - Jl. Margonda No. 7': 
-	   {
-	    'rating': 7.6, 
-	    'review': 9, 
-	    'address': "Jl. Margonda No.7, Depok"
-	   }
-	}
-
-	lovely_dummy_foody = {'KFC - Pondok Indah': 
-	   {
-	   	'foody_name': "KFC - Pondok Indah Mall 2",
-	   	'rating': 7.6, 
-	    'review': 11, 
-	    'address': "Pondok Indah Mall 2, Lt. 3, No. 14"
-	   },
-	 'McDonald - Jl. Margonda No. 23': 
-	   {
-	   	'foody_name': "McDonald - Margonda",
-	   	'rating': 7.6, 
-	    'review': 12, 
-	    'address': "Jl. Margonda No. 23, Beji, Depok"
-	   },
-	 'Pizza Hut - Jl. Margonda No. 7': 
-	   {
-	   	'foody_name': "Pizza Hut - Margonda",
-	    'rating': 7.6, 
-	    'review': 13, 
-	    'address': "Jl. Margonda No.7, Depok"
-	   }
-	}
-
-	lovely_dummy_pergikuliner = {'KFC - Pondok Indah': 
-	   {'rating': 7.5, 
-	    'review': 4, 
-	    'address': "Pondok Indah Mall 2, Lt. 3, No. 14"
-	   },
-	 'McDonald - Jl. Margonda No. 23': 
-	   {'rating': 5, 
-	    'review': 5, 
-	    'address': "Jl. Margonda No. 23, Beji, Depok"
-	   },
-	 'Pizza Hut - Jl. Margonda No. 7': 
-	   {
-	    'rating': 2.5, 
-	    'review': 6, 
-	    'address': "Jl. Margonda No.7, Depok"
-	   },
-	 'Pizza Hut - Jl. Lenteng Agung No. 2': 
-	   {
-	    'rating': 5, 
-	    'review': 7, 
-	    'address': "Jl. Lenteng Agung No.2, Jakarta Selatan"
-	   }
-	}
-
-	#result = result_zomato
-	result = lovely_dummy_zomato
-	for key,value in lovely_dummy_openrice.items():
+	result = result_zomato
+	for key,value in result_openrice.items():
 		if key in result.keys():
 			result[key]['no_of_occurences'] = 2
 			result[key]['rating'] += value['rating']
@@ -102,7 +23,7 @@ def integrate(query):
 			result[key] = value
 			result[key]['no_of_occurences'] = 1
 
-	for key,value in lovely_dummy_foody.items():
+	for key,value in result_foody.items():
 		if key in result.keys():
 			result[key]['no_of_occurences'] += 1
 			result[key]['rating'] += value['rating']
@@ -112,7 +33,7 @@ def integrate(query):
 			result[key] = value
 			result[key]['no_of_occurences'] = 1
 
-	for key,value in lovely_dummy_pergikuliner.items():
+	for key,value in result_pergikuliner.items():
 		if key in result.keys():
 			result[key]['no_of_occurences'] += 1
 			result[key]['rating'] += value['rating']
